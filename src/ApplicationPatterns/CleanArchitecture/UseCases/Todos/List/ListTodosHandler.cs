4aed1a80059b11203fd80cb5bc56e5f23d770298
Ivan Gajic <ivan.m.gajic@hotmail.com>
2025-03-28 14:51:21 +0100

    Explaining better when to put interface to use case layer.

--- src/ApplicationPatterns/CleanArchitecture/UseCases/Todos/List/ListTodosHandler.cs
+++ src/ApplicationPatterns/CleanArchitecture/UseCases/Todos/List/ListTodosHandler.cs
@@ -13,6 +13,8 @@ public class ListTodosHandler(IRepository<ToDo> repository)
     // If we need some much more complex query than basic repository fetches, we can create here in List folder interface for service
     // e.g. IListTodosRelatedToTopicQueryService and implement it inside Infrastructure/Database/QueryServices/ListTodosRelatedToTopicQueryService.cs.
     // We can in that service use any way to fetch data (ado, dapper, ef...)
+    // If you're querying for a list of todos to display to the user, filtered by criteria such as due topic, date or priority, and this query is only used within a specific application feature (such as showing tasks in a UI),
+    // then the interface belongs here, in the use case layer.
     public async Task<Result<IEnumerable<TodoDto>>> Handle(ListTodosQuery request, CancellationToken cancellationToken)
         => Result<IEnumerable<TodoDto>>.Success((await repository.ListAsync(cancellationToken)).Select(todo => new TodoDto(todo.Id, todo.IsResolved, todo.Description)));
 }
